6b6fe90c7f570a895a30d2e86622e7fd9e8ac80f
oldman <445471528@qq.com>
2019-12-06 19:32:07 +0800

    add sub module

---
+++ Const.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LockStepServer.LockStepFrameWork
+{
+    class Const
+    {
+        // 每秒逻辑帧次数
+        public static readonly int Frame_Rate = 30;
+
+        // 每秒逻辑帧长度
+        public static readonly float Frame_Length = Frame_Rate / 1000f;
+    }
+}
