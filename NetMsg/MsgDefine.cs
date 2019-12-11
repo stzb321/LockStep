2f9ef4004d6d3cf8991c93f2d182b683db70c0f2
oldman <445471528@qq.com>
2019-12-11 20:46:43 +0800

    add sub module

---
+++ NetMsg/MsgDefine.cs
@@ -0,0 +1,38 @@
+﻿using LockStepFrameWork.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LockStepFrameWork.NetMsg
+{
+    public interface ISerializable
+    {
+        void Serialize(Serializer writer);
+
+        void Deserialize(Deserializer reader);
+    }
+
+    [System.Serializable]
+    public class BaseFormater : ISerializable
+    {
+        public virtual void Deserialize(Deserializer reader)
+        {
+            
+        }
+
+        public virtual void Serialize(Serializer writer)
+        {
+            
+        }
+    }
+
+    public class BaseMsg : BaseFormater
+    {
+
+    }
+
+    public class PingMsg : BaseMsg
+    {
+        public int Tick;
+    }
+}
